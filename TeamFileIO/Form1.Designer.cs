fc0fa3d37751b4cb3feec5968d072ea25efc0048
rathbun518 <rathbun518@users.noreply.github.com>
2017-08-09 23:23:40 -0700

    Created form and added button functionality

--- TeamFileIO/Form1.Designer.cs
+++ TeamFileIO/Form1.Designer.cs
@@ -28,21 +28,153 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.txtFilename = new System.Windows.Forms.TextBox();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.txtFileContents = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtEncryptedFileContents = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtDecryptedFileContents = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(16, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(258, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select a file to encrypt/decrypt:";
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(20, 43);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(88, 35);
+            this.btnOpenFile.TabIndex = 1;
+            this.btnOpenFile.Text = "Open File";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // txtFilename
+            // 
+            this.txtFilename.Location = new System.Drawing.Point(124, 51);
+            this.txtFilename.Name = "txtFilename";
+            this.txtFilename.Size = new System.Drawing.Size(247, 20);
+            this.txtFilename.TabIndex = 2;
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(63, 99);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(113, 35);
+            this.btnEncrypt.TabIndex = 3;
+            this.btnEncrypt.Text = "Encrypt and Save";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(204, 99);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(113, 35);
+            this.btnDecrypt.TabIndex = 4;
+            this.btnDecrypt.Text = "Decrypt and Save";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // txtFileContents
+            // 
+            this.txtFileContents.Location = new System.Drawing.Point(20, 179);
+            this.txtFileContents.Multiline = true;
+            this.txtFileContents.Name = "txtFileContents";
+            this.txtFileContents.Size = new System.Drawing.Size(351, 73);
+            this.txtFileContents.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 163);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Original File Contents:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 269);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(122, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Encrypted File Contents:";
+            // 
+            // txtEncryptedFileContents
+            // 
+            this.txtEncryptedFileContents.Location = new System.Drawing.Point(20, 285);
+            this.txtEncryptedFileContents.Multiline = true;
+            this.txtEncryptedFileContents.Name = "txtEncryptedFileContents";
+            this.txtEncryptedFileContents.Size = new System.Drawing.Size(351, 73);
+            this.txtEncryptedFileContents.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 373);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(123, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Decrypted File Contents:";
+            // 
+            // txtDecryptedFileContents
+            // 
+            this.txtDecryptedFileContents.Location = new System.Drawing.Point(20, 391);
+            this.txtDecryptedFileContents.Multiline = true;
+            this.txtDecryptedFileContents.Name = "txtDecryptedFileContents";
+            this.txtDecryptedFileContents.Size = new System.Drawing.Size(351, 73);
+            this.txtDecryptedFileContents.TabIndex = 10;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(393, 479);
+            this.Controls.Add(this.txtDecryptedFileContents);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtEncryptedFileContents);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtFileContents);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.txtFilename);
+            this.Controls.Add(this.btnOpenFile);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Text = "File I/O";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.TextBox txtFilename;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.TextBox txtFileContents;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtEncryptedFileContents;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtDecryptedFileContents;
     }
 }
 
--- TeamFileIO/Form1.cs
+++ TeamFileIO/Form1.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,9 +19,73 @@ namespace TeamFileIO
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void btnOpenFile_Click(object sender, EventArgs e)
         {
+            var openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "Open Text File";
+            openFileDialog.Filter = "TXT files|*.txt";
 
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamReader fileIn = new StreamReader(openFileDialog.FileName))
+                {
+                    txtFilename.Text = openFileDialog.FileName;
+                    string fileContents = fileIn.ReadToEnd();
+                    txtFileContents.Text = fileContents;
+                }
+            }
+            else
+            {
+                MessageBox.Show("File could not be opened.");
+            }
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            //TODO Insert encrypt code
+
+            //TODO Update txtFileContents.Text to show the encrypted data
+            txtEncryptedFileContents.Text = "This is encrypted text";
+
+            //Save file as "encrypted_filename.txt"
+            SaveFileDialog savefile = new SaveFileDialog();
+            savefile.Title = "Save Text File";
+            savefile.FileName = "encrypted_file.txt";
+            savefile.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+
+            if (savefile.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamWriter sw = new StreamWriter(savefile.FileName))
+                    sw.Write(txtEncryptedFileContents.Text);
+            }
+            else
+            {
+                MessageBox.Show("File could not be saved.");
+            }
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            //TO Insert decrypt code
+
+            //TODO Update txtFileContents.Text to show the decrypted data
+            txtDecryptedFileContents.Text = "This is decrypted text";
+
+            //Save file as "decrypted_filename.txt"
+            SaveFileDialog savefile = new SaveFileDialog();
+            savefile.Title = "Save Text File";
+            savefile.FileName = "decrypted_file.txt";
+            savefile.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+
+            if (savefile.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamWriter sw = new StreamWriter(savefile.FileName))
+                    sw.Write(txtDecryptedFileContents.Text);
+            }
+            else
+            {
+                MessageBox.Show("File could not be saved.");
+            }
         }
     }
 }
